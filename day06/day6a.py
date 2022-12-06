776a1cfcc63eec5ddd2bdfa41bdad02cc56d77da
Shaina Peters <darthbeep@gmail.com>
2022-12-06 09:06:06 -0500

    golfed more on day 6

--- day06/day6a.py
+++ day06/day6a.py
@@ -1,3 +1,3 @@
-i = [x for x in ((open("day06/input", "r")).read()[:-1])]
-r=[x+4 for x in range(len(i)-4) if len(set(i[x:x+4]))==len(i[x:x+4])][0]
+i=open("day06/input", "r").read()[:-1]
+r=[x for x in range(4,len(i)) if len(set(i[x-4:x]))==4][0]
 print(r)
--- day06/day6b.py
+++ day06/day6b.py
@@ -1,3 +1,3 @@
-i = [x for x in ((open("day06/input", "r")).read()[:-1])]
-r=[x+14 for x in range(len(i)-14) if len(set(i[x:x+14]))==len(i[x:x+14])][0]
+i=open("day06/input", "r").read()[:-1]
+r=[x for x in range(14,len(i)) if len(set(i[x-14:x]))==14][0]
 print(r)
